09af33421514a1e1ff8ea7e5273db38bdac271c1
xl04d <aurelien@xload.io>
2014-09-30 15:36:16 +0200

    solution.py

---
+++ exercices/006/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien
+Mail       : aurelien@hackers.camp
+Project    : print hello world 42 using list
+Created on : Tue Sep 30 08:39:03 2014
+
+References
+ [argv](https://docs.python.org/3.4/library/sys.html)
+ [import](https://docs.python.org/3/reference/simple_stmts.html#import)
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+hello = ["Hello world", 42]
+print(hello)
