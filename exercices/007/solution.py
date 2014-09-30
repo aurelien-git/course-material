d51ef78cb7ce0ad5fb590eb60548d3faf505dd45
xl04d <aurelien@xload.io>
2014-09-30 16:35:10 +0200

    solution.py

---
+++ exercices/007/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien
+Mail       : aurelien@hackers.camp
+Project    : print vertically
+Created on : Tue Sep 30 15:59:30 2014
+
+References
+ https://docs.python.org/3.4/reference/compound_stmts.html#for (for loop)
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+
+for i in 'Hello World !':
+    print(i)
