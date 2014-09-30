2b6be0fc905532835c6df2554c047f91e988e0c1
xl04d <aurelien@xload.io>
2014-09-30 15:23:45 +0200

    solution.py

---
+++ exercices/000/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIÈRES
+Mail       : aurelien@hackers.camp
+Project    : print "Hello World".
+Created on : Tue Sep 30 08:39:03 2014
+
+References
+ [argv](https://docs.python.org/3.4/library/sys.html)
+ [import](https://docs.python.org/3/reference/simple_stmts.html#import)
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+print('"Hello World".')
