cf5e7d7839cdd18e4b2d1be6edcce5fc8651bb81
xl04d <aurelien@xload.io>
2014-09-30 18:13:11 +0200

    solution.py

---
+++ exercices/008/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien
+Mail       : aurelien@hackers.camp
+Project    : Number of character in a paragraph
+Created on : Tue Sep 30 15:59:30 2014
+
+References
+ https://docs.python.org/3/library/functions.html#len
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+phantom_menace = """Turmoil has engulfed the Galactic Republic. The\
+ taxation of trade routes to outlying star systems is in\
+ dispute. Hoping to resolve the matter with a blockade of deadly\
+ battleships, the greedy Trade Federation has stopped all shipping to\
+ the small planet of Naboo. While the congress of the Republic\
+ endlessly debates this alarming chain of events, the Supreme\
+ Chancellor has secretly dispatched two Jedi Knights, the guardians of\
+ peace and justice in the galaxy, to settle the conflict"""
+print(len(phantom_menace))
