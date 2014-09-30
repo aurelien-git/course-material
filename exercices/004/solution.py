9f7466e5f342a2ca92890bee3528c4182e9276df
xl04d <aurelien@xload.io>
2014-09-30 15:34:32 +0200

    solution.py

---
+++ exercices/004/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien
+Mail       : aurelien@hackers.camp
+Project    : find the name of the script and print it
+Created on : Tue Sep 30 15:34:06 2014
+
+References
+ [argv](https://docs.python.org/3.4/library/sys.html)
+ [import](https://docs.python.org/3/reference/simple_stmts.html#import)
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+import sys
+print(sys.argv[0])
