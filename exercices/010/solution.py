173b92295bc2dae67b699e8849eeb7f383a59af3
xl04d <aurelien@xload.io>
2014-10-01 09:14:28 +0200

    solution.py

---
+++ exercices/010/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIERES
+Mail       : aurelien@hackers.camp
+Project    : Print one hundred time a word (one per line)
+Created on : Tue Sep 30 19:12:02 2014
+
+References
+
+https://docs.python.org/3/tutorial/controlflow.html#for-statements
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+
+for i in range(1, 100):
+    print ('Hello World')
