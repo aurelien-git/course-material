8c26d618f207749e95d607969ec8aa3607b5cfe2
xl04d <aurelien@xload.io>
2014-10-01 10:05:51 +0200

    solution.py

---
+++ exercices/012/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIERES
+Mail       : aurelien@hackers.camp
+Project    : What python think of a mathematique comparaison
+Created on : Wed Oct  1 09:15:59 2014
+
+References
+
+https://docs.python.org/3/library/stdtypes.html#comparisons
+
+Course material at
+https://github.com/hackinscience/hackinscience.github.io/wiki/Start-Here
+
+"""
+
+if ('2 + 2 != 4'):
+    print(False)
